c41b43843bc16e837679f97b2661044c6c77ddf4
DanielBonnery <daniel.bonnery@gmail.com>
2015-09-21 12:34:20 -0400

    a

--- R/removetikzfile.R
+++ R/removetikzfile.R
@@ -1,11 +1,13 @@
-removetikzfile <-function(tmpfile,scale=1,yxratio=1){
+removetikzfile <-function(tmpfile,scale=c(1,1),yxratio=c(1,1)){
+  if(length(scale)==1){scale=rep(scale,2)}
+  if(length(yxratio)==1){scale=rep(yxratio,2)}
   ww<-readLines(file(tmpfile))
   ww<-ww[!sapply(ww, function(x){any(grep("%",x)==1)})]
   xx<-paste(ww,collapse="\n")  
-xx<-paste0("\\redefyxtikzratio{",yxratio,"}
+xx<-paste0("\\redefyxtikzratio{",yxratio[1],"}{",yxratio[2],"}
              ",xx,"\\redefyxtikzratio{1}")
-  xx<-paste0("\\rescale{",scale,"}
-             ",xx,"\\rescale{1}")
+  xx<-paste0("\\rescale{",scale[1],"}{",scale[2],"}
+             ",xx,"\\rescale{1}{1}")
   yy<-gsub('\\','\\\\', xx, fixed=TRUE)
   yy<-gsub('x=1pt,y=1pt',paste0('x=1pt,y=1pt,scale=\\\\tikzscale'), yy, fixed=TRUE)
   yy<-gsub('x=1pt,y=1pt','x=1pt,y=\\\\tikzsyxratio pt', yy, fixed=TRUE)
