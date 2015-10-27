942d9b01341d417e312d1b1f2da82e202dea58d7
DanielBonnery <daniel.bonnery@gmail.com>
2015-10-26 22:43:26 -0400

    a

--- R/graphtikzcode.R
+++ R/graphtikzcode.R
@@ -1,14 +1,14 @@
 graphtikzcode <-
-function(texte,widthe=7,heighte=7,scale=c(1,1),yxratio=c(1,1),...){
+function(texte,widthe=7,heighte=7,scale=c(1,1),yxratio=c(1,1),caption=NULL,label=NULL,addfigureenv=FALSE,sanitize=FALSE,...){
   par()
   if(!require("tikzDevice")){devtools::install_github("yihui/tikzDevice")}
   require("tikzDevice")
   documentDeclaration<-1
   getDocumentPointsize<-function(a){12}
   tmpfile=paste0(tempfile(),".tex")
-  tikz(file=tmpfile, standAlone = FALSE,sanitize=FALSE,width=widthe,height=heighte, footer = getOption("tikzFooter"),
+  tikz(file=tmpfile, standAlone = FALSE,sanitize=sanitize,width=widthe,height=heighte, footer = getOption("tikzFooter"),
        packages=
          c(options()$tikzLatexPackages,"\\usepackage{amsfonts}","\\usepackage{amssymb}"),...)  
   eval.parent(parse(text=texte),n=1)
   dev.off()  
-  return(removetikzfile(tmpfile,scale=scale,yxratio=yxratio))}
+  return(removetikzfile(tmpfile,scale=scale,yxratio=yxratio,caption=caption,label=label,addfigureenv=addfigureenv))}
--- R/removetikzfile.R
+++ R/removetikzfile.R
@@ -1,4 +1,4 @@
-removetikzfile <-function(tmpfile,scale=c(1,1),yxratio=c(1,1)){
+removetikzfile <-function(tmpfile,scale=c(1,1),yxratio=c(1,1),caption=NULL,label=NULL,addfigureenv=NULL){
   if(length(scale)==1){scale=rep(scale,2)}
   if(length(yxratio)==1){yxratio=rep(yxratio,2)}
   ww<-readLines(file(tmpfile))
@@ -11,5 +11,12 @@ xx<-paste0("\\redeftikzyxratio{",yxratio[1],"}{",yxratio[2],"}
   yy<-gsub('\\','\\\\', xx, fixed=TRUE)
   yy<-gsub('x=1pt,y=1pt',paste0('x=1pt,y=1pt,scale=\\\\tikzscale'), yy, fixed=TRUE)
   yy<-gsub('x=1pt,y=1pt','x=1pt,y=\\\\tikzyxratio pt', yy, fixed=TRUE)
+if(addfigureenv|!is.null(caption)|!is.null(label)){
+  yy<-paste0("\\\\begin{figure}[H]",
+             if(!is.null(caption)){paste0("\\\\caption{",caption,"}")}else{character(0)},
+if(!is.null(label)){paste0("\\\\caption{",label,"}")}}else{character(0)},
+yy,
+"\\\\end{figure}")
+
   file.remove(tmpfile)
   return(yy)}
