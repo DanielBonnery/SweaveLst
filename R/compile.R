f1561b8f95e97b09e37d2c6492927ff148636db2
DanielBonnery <daniel.bonnery@gmail.com>
2016-03-16 22:59:37 -0400

    a

--- R/compile.R
+++ R/compile.R
@@ -1,13 +1,24 @@
 compile<-function(filename,directory=getwd()){
   nettoie(directory)
   #if(.Platform$OS.type == "unix") {} else {}
-  system(paste0("pdflatex '",file.path(directory,filename),".tex' -shell-escape -draftmode -interaction=nonstopmode;"),
-         show.output.on.console = FALSE,wait=FALSE,ignore.stdout=TRUE, ignore.stderr=TRUE)
-  system(paste0("bibtex -terse '",file.path(directory,filename),".aux'"),
-         show.output.on.console = FALSE,wait=FALSE,ignore.stdout=TRUE, ignore.stderr=TRUE)
-  system(paste0("pdflatex '",file.path(directory,filename),".tex' -interaction=nonstopmode;"),
-         show.output.on.console = FALSE,wait=FALSE,ignore.stdout=TRUE, ignore.stderr=TRUE)
-  system(paste0("pdflatex '",file.path(directory,filename),".tex' -interaction=nonstopmode;"),
-         show.output.on.console = FALSE,wait=FALSE,ignore.stdout=TRUE, ignore.stderr=TRUE)
+  system(command = paste0("
+pdflatex '",file.path(directory,filename),".tex' -shell-escape -draftmode -interaction=batchmode;
+bibtex -terse '",file.path(directory,filename),".aux';
+pdflatex '",file.path(directory,filename),".tex' -shell-escape -interaction=batchmode;
+pdflatex '",file.path(directory,filename),".tex' -shell-escape -interaction=batchmode;"),
+         intern=FALSE,wait=FALSE,
+         ignore.stdout=TRUE, 
+         ignore.stderr=TRUE)
+  
+  system2("pdflatex -shell-escape -draftmode -interaction=nonstopmode ",
+          args=
+pdflatex '",file.path(directory,filename),".tex' -shell-escape -draftmode -interaction=nonstopmode;
+bibtex -terse '",file.path(directory,filename),".aux';
+pdflatex '",file.path(directory,filename),".tex' -interaction=nonstopmode;
+pdflatex '",file.path(directory,filename),".tex' -interaction=nonstopmode;"),
+         wait=FALSE,
+         stdout=FALSE, 
+         stderr=FALSE)
+  
   nettoie(directory)
 }
