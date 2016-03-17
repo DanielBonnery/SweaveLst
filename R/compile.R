d35e97e793b5055c042f69c9b79402a4168570a0
DanielBonnery <daniel.bonnery@gmail.com>
2016-03-16 22:30:30 -0400

    a

--- R/compile.R
+++ R/compile.R
@@ -1,12 +1,13 @@
 compile<-function(filename,directory=getwd()){
   nettoie(directory)
   #if(.Platform$OS.type == "unix") {} else {}
-    system(paste0(
-      "cd '",directory,"';
-      pdflatex '",filename,".tex' -shell-escape -draftmode -interaction=nonstopmode; 
-      bibtex -terse '",filename,".aux';
-      pdflatex '",filename,".tex' -interaction=nonstopmode;
-      pdflatex '",filename,".tex' -interaction=nonstopmode;}  
-    "),wait=FALSE,ignore.stdout=TRUE, ignore.stderr=TRUE)
+  system(paste0("pdflatex '",file.path(directory,filename),".tex' -shell-escape -draftmode -interaction=nonstopmode;"),
+         show.output.on.console = FALSE,wait=FALSE,ignore.stdout=TRUE, ignore.stderr=TRUE)
+  system(paste0("bibtex -terse '",file.path(directory,filename),".aux'"),
+         show.output.on.console = FALSE,wait=FALSE,ignore.stdout=TRUE, ignore.stderr=TRUE)
+  system(paste0("pdflatex '",file.path(directory,filename),".tex' -interaction=nonstopmode;")
+         show.output.on.console = FALSE,wait=FALSE,ignore.stdout=TRUE, ignore.stderr=TRUE)
+  system(paste0("pdflatex '",file.path(directory,filename),".tex' -interaction=nonstopmode;"),
+         show.output.on.console = FALSE,wait=FALSE,ignore.stdout=TRUE, ignore.stderr=TRUE)
   nettoie(directory)
 }
