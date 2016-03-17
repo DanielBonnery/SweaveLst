d5ff7e47cc19749a7d259438ceffbda2bb94201a
DanielBonnery <daniel.bonnery@gmail.com>
2016-03-16 23:00:03 -0400

    a

--- R/compile.R
+++ R/compile.R
@@ -10,15 +10,5 @@ pdflatex '",file.path(directory,filename),".tex' -shell-escape -interaction=batc
          ignore.stdout=TRUE, 
          ignore.stderr=TRUE)
   
-  system2("pdflatex -shell-escape -draftmode -interaction=nonstopmode ",
-          args=
-pdflatex '",file.path(directory,filename),".tex' -shell-escape -draftmode -interaction=nonstopmode;
-bibtex -terse '",file.path(directory,filename),".aux';
-pdflatex '",file.path(directory,filename),".tex' -interaction=nonstopmode;
-pdflatex '",file.path(directory,filename),".tex' -interaction=nonstopmode;"),
-         wait=FALSE,
-         stdout=FALSE, 
-         stderr=FALSE)
-  
   nettoie(directory)
 }
