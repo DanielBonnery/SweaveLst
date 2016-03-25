df23813863f4919d0d5d0dd5f7f37f53ff32b84f
DanielBonnery <daniel.bonnery@gmail.com>
2016-03-25 08:56:47 -0400

    a

--- R/compile.R
+++ R/compile.R
@@ -2,7 +2,7 @@ compile<-function(filename,directory=getwd()){
   nettoie(directory)
   #if(.Platform$OS.type == "unix") {} else {}
   system(command = paste0("
-pdflatex '",file.path(directory,filename),".tex' -shell-escape -draftmode -interaction=batchmode;
+pdflatex '",file.path(directory,filename),".tex' -shell-escape -interaction=batchmode;
 bibtex -terse '",file.path(directory,filename),".aux';
 pdflatex '",file.path(directory,filename),".tex' -shell-escape -interaction=batchmode;
 pdflatex '",file.path(directory,filename),".tex' -shell-escape -interaction=batchmode;"),
