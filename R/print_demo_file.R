6f07e41cedd1c89645bba73589b6d8d781cc0dcd
daniel <daniel.bonnery@gmail.com>
2018-09-17 12:57:26 -0400

    a

---
+++ R/print_demo_file.R
@@ -0,0 +1,17 @@
+#' Gives the tex code to print a demo code
+#'
+#'@param topic
+#'@param package
+#'@example 
+#'topic="nlm"
+#'package="stats"
+#'print_demo_file(topic,package)
+
+
+print_demo_file<-function(topic,package){
+  paste0(
+    "\\textbf{Rcode for ",package,"::",topic,"}\n",
+    "\\begin{lstlisting}[style=Rinput]\n",
+    paste(readLines(system.file("demo",paste0(topic,".R"),package=package)),
+          collapse="\n"),
+    "\\end{lstlisting}\n")}
