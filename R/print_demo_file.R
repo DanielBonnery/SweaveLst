5e9c13e223a7e37d1c03fda942e935daad270826
daniel <daniel.bonnery@gmail.com>
2018-10-08 20:12:04 -0400

    a

--- R/print_demo_file.R
+++ R/print_demo_file.R
@@ -11,7 +11,8 @@
 print_demo_file<-function(topic,package){
   paste0(
     "\\\\textbf{Rcode for ",package,"::",topic,"}\n",
-    "\\\\begin{lstlisting}[style=Rinput,title='demo: ",topic,"package: ",package,"']\n",
+    "\\\\begin{lstlisting}[style=Rinput,title={demo: ",topic," - package: ",package,"}]\n",
     paste(readLines(system.file("demo",paste0(topic,".R"),package=package)),
           collapse="\n"),
     "\\\\end{lstlisting}\n")}
+
