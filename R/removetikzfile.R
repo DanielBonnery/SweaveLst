d9f4b8dc526cc005a1b7bb6a30c068de26470cd5
daniel <daniel.bonnery@gmail.com>
2021-05-10 11:34:58 +0100

    No information

--- R/removetikzfile.R
+++ R/removetikzfile.R
@@ -30,26 +30,26 @@ removetikzfile <-function(tmpfile,
   ww<-ww[!sapply(ww, function(x){any(grep("%",x)==1)})]
   xx<-paste(ww,collapse="\n")
   if(any(yxratio!=1)){
-  xx<-paste0("\\redeftikzyxratio{",yxratio[1],"}{",yxratio[2],"}
+    xx<-paste0("\\redeftikzyxratio{",yxratio[1],"}{",yxratio[2],"}
              ",xx,"\\redeftikzyxratio{1}{1}")}
   if(any(scale!=1)){
     xx<-paste0("\\rescale{",scale[1],"}{",scale[2],"}
              ",xx,"\\rescale{1}{1}")}
-    yy<-iconv("utf-8", "utf-8//ignore", xx);
-    yy<-gsub('\\','\\\\', yy, fixed=TRUE)
-    yy<-gsub('x=1pt,y=1pt',paste0('x=1pt,y=1pt,scale=\\\\tikzscale'), yy, fixed=TRUE)
-    yy<-gsub('x=1pt,y=1pt','x=1pt,y=\\\\tikzyxratio pt', yy, fixed=TRUE)
-    yy<-gsub(';\n', '; ', yy, fixed=TRUE)
-    yy<-gsub('--\n', '-- ', yy, fixed=TRUE) 
-    yy<-gsub('\n\n', '\n', yy, fixed=TRUE)
-    if(addfigureenv|!is.null(caption)|!is.null(label)){
-      yy<-paste0("\\\\begin{figure}[H]",
-                 if(!is.null(caption)){paste0("\\\\caption{",caption,"}")}else{character(0)},
-                 if(!is.null(label)){paste0("\\\\label{",label,"}")}else{character(0)},
-                 yy,
-                 "\\\\end{figure}")}
-    if(!is.null(modify)){
-      yy<-modify(yy)}
+  yy<-iconv(xx, from="UTF-8", to="UTF-8",sub='');#remove all non utf8 characters
+  yy<-gsub('\\','\\\\', yy, fixed=TRUE)
+  yy<-gsub('x=1pt,y=1pt',paste0('x=1pt,y=1pt,scale=\\\\tikzscale'), yy, fixed=TRUE)
+  yy<-gsub('x=1pt,y=1pt','x=1pt,y=\\\\tikzyxratio pt', yy, fixed=TRUE)
+  yy<-gsub(';\n', '; ', yy, fixed=TRUE)
+  yy<-gsub('--\n', '-- ', yy, fixed=TRUE) 
+  yy<-gsub('\n\n', '\n', yy, fixed=TRUE)
+  if(addfigureenv|!is.null(caption)|!is.null(label)){
+    yy<-paste0("\\\\begin{figure}[H]",
+               if(!is.null(caption)){paste0("\\\\caption{",caption,"}")}else{character(0)},
+               if(!is.null(label)){paste0("\\\\label{",label,"}")}else{character(0)},
+               yy,
+               "\\\\end{figure}")}
+  if(!is.null(modify)){
+    yy<-modify(yy)}
   return(yy)}
 
 
