38e2adf05444b5c4e26e7dd602343f67dafc217d
Daniel Bonnéry <dbb31@cam.ac.uk>
2026-09-19 20:28:37 +0200

    Escape underscores in R file content display

--- R/affiche_contenu_fichier_r.r
+++ R/affiche_contenu_fichier_r.r
@@ -14,6 +14,7 @@
 affiche_contenu_fichier_r <- function(fichier) {
     lines <- readLines(fichier)
     escaped_lines <- gsub("#", "\\\\#", lines, fixed = TRUE)
+    escaped_lines <- gsub("_", "\\\\_", escaped_lines, fixed = TRUE)
     paste0("\\textbf{Code R}\n",
            "\\begin{lstlisting}[style=Rinput,title={", fichier, "},captionpos=tb]\n",
            paste(escaped_lines, collapse = "\n"),
