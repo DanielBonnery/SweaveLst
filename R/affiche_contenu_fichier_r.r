9348c1da3e8eebaadfbda453fdd96b6b64601f4a
Daniel Bonnéry <dbb31@cam.ac.uk>
2026-09-19 20:45:34 +0200

    Update affiche_contenu_fichier_r.r

--- R/affiche_contenu_fichier_r.r
+++ R/affiche_contenu_fichier_r.r
@@ -17,8 +17,8 @@ affiche_contenu_fichier_r <- function(fichier) {
     escaped_lines <- gsub("#", "\\\\#", lines, fixed = TRUE)
     escaped_lines <- gsub("_", "\\\\_", escaped_lines, fixed = TRUE)
     paste0("\\\\textbf{Code R}\\n",
-           "\\\\begin{lstlisting}[style=Rinput,title={", fichier, "},captionpos=tb]\\n",
-           paste(escaped_lines, collapse = "\\n"),
-           "\\\\end{lstlisting}\\n")
+           "\\\\begin{lstlisting}[style=Rinput,title={", fichier, "},captionpos=tb]\n",
+           paste(escaped_lines, collapse = "\n"),
+           "\\\\end{lstlisting}\n")
 }
 
